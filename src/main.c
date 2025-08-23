bce9e835fa6602c86cd3e51173a4f2858f38063b
Jerome Abela <Jerome.Abela@gmail.com>
2025-08-23 13:15:24 +0100

    Implement dialog to create new projects

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include "evaluate.c"
 #include "file_open.c"
 #include "file_new.c"
+#include "project_new_wizard.c"
 #include "file_save.c"
 #include "gtk_text_provider.c"
 #include "interactions_view.c"
---
+++ src/project_new_wizard.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <gtk/gtk.h>
+
+void project_new_wizard(GtkWidget *widget, gpointer data);
+
