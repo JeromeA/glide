c1e92181b934d83fd74ccf196c4338f35f95e25b
Jerome Abela <Jerome.Abela@gmail.com>
2025-08-23 23:24:49 +0100

    Sync notebook and ASDF selections

--- src/asdf_view.c
+++ src/asdf_view.c
@@ -11,6 +11,8 @@ G_DEFINE_TYPE(AsdfView, asdf_view, GTK_TYPE_TREE_VIEW)
 enum { COL_TEXT, ASDF_VIEW_N_COLS };
 
 static void asdf_view_populate_store(AsdfView *self);
+static gboolean filename_matches(const gchar *component, const gchar *file);
+static gchar *get_selected_component(AsdfView *self);
 
 static void
 asdf_view_init(AsdfView *self)
@@ -89,5 +91,89 @@ asdf_view_new(Asdf *asdf)
   self->asdf = g_object_ref(asdf);
   asdf_view_populate_store(self);
   return GTK_WIDGET(self);
+}
+
+static gboolean
+filename_matches(const gchar *component, const gchar *file)
+{
+  if (!component || !file)
+    return FALSE;
+  if (g_strcmp0(component, file) == 0)
+    return TRUE;
+  gchar *base = g_path_get_basename(file);
+  gchar *dot = g_strrstr(base, ".");
+  if (dot)
+    *dot = '\0';
+  gboolean match = g_strcmp0(component, base) == 0;
+  g_free(base);
+  return match;
+}
+
+static gchar *
+get_selected_component(AsdfView *self)
+{
+  GtkTreeSelection *selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(self));
+  GtkTreeModel *model;
+  GtkTreeIter iter;
+  if (!gtk_tree_selection_get_selected(selection, &model, &iter))
+    return NULL;
+  GtkTreeIter parent;
+  if (!gtk_tree_model_iter_parent(model, &parent, &iter))
+    return NULL;
+  gchar *parent_text = NULL;
+  gtk_tree_model_get(model, &parent, COL_TEXT, &parent_text, -1);
+  gboolean is_component = g_strcmp0(parent_text, "components") == 0;
+  g_free(parent_text);
+  if (!is_component)
+    return NULL;
+  gchar *comp = NULL;
+  gtk_tree_model_get(model, &iter, COL_TEXT, &comp, -1);
+  return comp;
+}
+
+void
+asdf_view_select_file(AsdfView *self, const gchar *file)
+{
+  g_return_if_fail(ASDF_IS_VIEW(self));
+  g_return_if_fail(file != NULL);
+  gchar *current = get_selected_component(self);
+  if (filename_matches(current, file)) {
+    g_free(current);
+    return;
+  }
+  g_free(current);
+  GtkTreeModel *model = GTK_TREE_MODEL(self->store);
+  GtkTreeIter root;
+  GtkTreeIter iter;
+  GtkTreeIter child;
+  if (!gtk_tree_model_get_iter_first(model, &root))
+    return;
+  if (!gtk_tree_model_iter_children(model, &iter, &root))
+    return;
+  do {
+    gchar *text = NULL;
+    gtk_tree_model_get(model, &iter, COL_TEXT, &text, -1);
+    gboolean is_components = g_strcmp0(text, "components") == 0;
+    g_free(text);
+    if (is_components) {
+      if (gtk_tree_model_iter_children(model, &child, &iter)) {
+        do {
+          gchar *comp = NULL;
+          gtk_tree_model_get(model, &child, COL_TEXT, &comp, -1);
+          if (filename_matches(comp, file)) {
+            GtkTreeSelection *sel = gtk_tree_view_get_selection(GTK_TREE_VIEW(self));
+            GtkTreePath *path = gtk_tree_model_get_path(model, &child);
+            gtk_tree_selection_select_path(sel, path);
+            gtk_tree_view_scroll_to_cell(GTK_TREE_VIEW(self), path, NULL, FALSE, 0, 0);
+            gtk_tree_path_free(path);
+            g_free(comp);
+            return;
+          }
+          g_free(comp);
+        } while (gtk_tree_model_iter_next(model, &child));
+      }
+      break;
+    }
+  } while (gtk_tree_model_iter_next(model, &iter));
 }
 
--- src/asdf_view.h
+++ src/asdf_view.h
@@ -9,5 +9,6 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE(AsdfView, asdf_view, ASDF, VIEW, GtkTreeView)
 
 GtkWidget *asdf_view_new(Asdf *asdf);
+void asdf_view_select_file(AsdfView *self, const gchar *file);
 
 G_END_DECLS
