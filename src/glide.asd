bfa373b8a182610467a161c0888ee6b2c677c20d
Jerome Abela <Jerome.Abela@gmail.com>
2025-08-24 21:35:04 +0100

    Add ASDF system listing package symbols

---
+++ src/glide.asd
@@ -0,0 +1,5 @@
+(asdf:defsystem "glide"
+  :description "List symbols of a given package"
+  :version "0.0.1"
+  :serial t
+  :components ((:file "symbols")))
---
+++ src/symbols.lisp
@@ -0,0 +1,11 @@
+(defpackage :glide
+  (:use :cl)
+  (:export :package-symbols))
+
+(in-package :glide)
+
+(defun package-symbols (package-name)
+  (let ((pkg (find-package package-name)))
+    (when pkg
+      (loop for symbol being the symbols of pkg
+            collect symbol))))
