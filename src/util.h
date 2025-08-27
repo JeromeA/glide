0f0748746a371138c8f57fd21051276167cea1ed
JeromeA <jerome.abela@gmail.com>
2025-08-27 12:35:15 +0100

    Increase debug_80 to 160.

--- src/util.h
+++ src/util.h
@@ -2,10 +2,10 @@
 
 #include <glib.h>
 
-static inline void g_debug_80(const char *string, const char *msg)
+static inline void g_debug_160(const char *string, const char *msg)
 {
-  if (strlen(msg) > 80)
-    g_debug("%s%.80s...", string, msg);
+  if (strlen(msg) > 160)
+    g_debug("%s%.160s...", string, msg);
   else
     g_debug("%s%s", string, msg);
 }
